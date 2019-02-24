05dd177a49e4086a971e335d20ef58997659c619
Paul <memo@sibnet.ru>
2019-02-24 11:53:13 +0300

    Simple identity moved to separate namespace

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import { Bootstrapper, Instance } from "@core";
-import { SimpleIdentityChecker, SimpleAuthModule, InMemoryUserStorage } from "@users";
+import { InMemoryUserStorage, SimpleIdentity } from "@users";
 import { ExpressFacadeFactory } from "@transport";
 
 function buildTestServer(): Instance {
     let bootstrapper = new Bootstrapper();
     bootstrapper
         .withHttpFacade(new ExpressFacadeFactory())
-        .withAuthMethod(new SimpleAuthModule(new SimpleIdentityChecker()))
-        .withStorage(new InMemoryUserStorage());
+        .withAuthMethod(new SimpleIdentity.SimpleAuthModule())
+        .withStorage(SimpleIdentity.WithSuperUser(new InMemoryUserStorage()));
 
     return bootstrapper.build();
 }
