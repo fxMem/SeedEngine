5695d7df8060ae0995a30e8e8fd742ed426512ef
Paul <memo@sibnet.ru>
2019-03-02 10:59:02 +0300

    changed client api to be more adequate

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -10,28 +10,64 @@ export type Connect = ConnectedClient & ServerApi;
 
 export class Client {
     private connection: Connection;
+    private client: ConnectedClient;
+    private messageCallback: any = (message) => {};
 
     constructor() {
         this.connection = new Connection(new SocketIOClientTransport());
     }
 
-    connect(callback: (connect: Connect) => Promise<void>) {
-        this.connection.onConnected((connectedClient) => {
-            let connect: Connect = { 
-                ...connectedClient,
-                getAvailableAuthMethods: () => connectedClient.invoke({
-                    header: Header.Authenticate, 
-                    payload: null
-                }),
-                authenticate: (moduleId, data) => connectedClient.invoke({
-                    header: Header.Authenticate, 
-                    payload: { moduleId, ...data }
-                })
-            };
+    connect(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.connection.onConnected((connectedClient) => {
+                this.client = connectedClient;
 
-            callback(connect);
+                connectedClient.onMessage((message) => {
+                    return this.messageCallback(message);
+                });
+                
+                resolve();
+            });
+    
+            this.connection.start();
         });
-
-        this.connection.start();
     }
+
+    onMessage(callback: (message) => {}): void {
+        this.messageCallback = callback;
+    }
+
+    authenticate(moduleId: string, data: any): Promise<any> {
+        return this.client.invoke({
+            header: Header.Authenticate, 
+            payload: { moduleId, ...data }
+        })
+    }
+
+    getAvailableAuthMethods(): Promise<any> {
+        return this.client.invoke({
+            header: Header.Authenticate, 
+            payload: null
+        })
+    } 
+
+    // connect1(callback: (connect: Connect) => Promise<void>) {
+    //     this.connection.onConnected((connectedClient) => {
+    //         let connect: Connect = { 
+    //             ...connectedClient,
+    //             getAvailableAuthMethods: () => connectedClient.invoke({
+    //                 header: Header.Authenticate, 
+    //                 payload: null
+    //             }),
+    //             authenticate: (moduleId, data) => connectedClient.invoke({
+    //                 header: Header.Authenticate, 
+    //                 payload: { moduleId, ...data }
+    //             })
+    //         };
+
+    //         callback(connect);
+    //     });
+
+    //     this.connection.start();
+    // }
 }
