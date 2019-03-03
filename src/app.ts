caea3205ac435ab573426a8cecfb7f37ec36ec26
Paul <faarseer@ya.ru>
2019-03-03 17:26:30 +0300

    added session handler to test config

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import { Bootstrapper, Instance } from "@core";
 import { InMemoryUserStorage, SimpleIdentity } from "@users";
-import { ExpressFacadeFactory } from "@transport";
+import { ExpressFacadeFactory, makeRegularHandler } from "@transport";
+import { SessionPipeline, DefaultSessionManager } from "@session";
 
 function buildTestServer(): Instance {
     let bootstrapper = new Bootstrapper();
     bootstrapper
         .withHttpFacade(new ExpressFacadeFactory())
         .withAuthMethod(new SimpleIdentity.SimpleAuthModule())
-        .withStorage(SimpleIdentity.WithSuperUser(new InMemoryUserStorage()));
+        .withStorage(SimpleIdentity.WithSuperUser(new InMemoryUserStorage()))
+        .add(makeRegularHandler(new SessionPipeline(new DefaultSessionManager())));
 
     return bootstrapper.build();
 }
