276d1f86fa7a3546d067bc5303ce8c62bc5c4396
Paul <faarseer@ya.ru>
2019-05-11 20:44:15 +0300

    added comment reasoning behind using disconnect context

--- src/transport/UserDisconnectedContext.ts
+++ src/transport/UserDisconnectedContext.ts
@@ -1,5 +1,9 @@
 import { MessageContext, Message, User, Users, MessageSender } from "../server";
 
+// This object passed to pipeline when user disconnects, naturally its
+// message property is null. There was another possibiilty to handle diconeectes - 
+// pass them via UserManager. It would requre though to implement separete wiring for
+// disconnect event 'subscribers' so now I take approach to reuse 'Pipeline's system
 export class UserDisconnectedContext implements MessageContext {
     message: Message;    
     
