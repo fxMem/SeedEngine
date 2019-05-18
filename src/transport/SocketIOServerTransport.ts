fa21e82b2cb6b30fdd0d5a5f804dd3e3e3a4b77c
Paul <faarseer@ya.ru>
2019-05-18 14:25:37 +0300

    disallow sending message wit empty targets list and without broadcast flag

--- src/transport/SocketIOServerTransport.ts
+++ src/transport/SocketIOServerTransport.ts
@@ -3,6 +3,7 @@ import { SeedMessage, DefaultSeedNamespace } from './Headers';
 import { HttpFacade } from './HttpFacade';
 import { Transport, TransportMessage, TransportClient } from './Transport';
 import { MessageTarget } from './MessageTarget';
+import { ServerError } from '../server';
 
 // Wraps socket.IO connection (server)
 export class SocketIOServerTransport implements Transport {
@@ -24,13 +25,18 @@ export class SocketIOServerTransport implements Transport {
 
     send(message: TransportMessage, options: { broadcast?: boolean, targets: MessageTarget[] }): void {
         let target = this.ioServer;
-
+        let targetsAdded = false;
         if (options.targets) {
             for (let clientTarget of options.targets) {
                 for (const targetId of clientTarget.targets) {
                     target = target.to(targetId);
+                    targetsAdded = true;
                 }
             }
+        }
+
+        if (!targetsAdded && !options.broadcast) {
+            throw new ServerError(`Cannot send message ${JSON.stringify(message)} because target-list is empty but message is not broadcast!`);
         }
         
         target.emit(SeedMessage, message);
